feaa992839fea219dc45df7e5f902f67dc228f53
liuzhihua0104 <liuzhihuarose@126.com>
2021-12-23 18:19:54 +0800

    首页整体布局

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,11 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Home',
-  //   component: Home
-  // },
+  {
+    path: '/',
+    name: 'home',
+    component: ()=>import("../views/Home.vue")
+  },
   // {
   //   path: '/about',
   //   name: 'About',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,67 @@
 <template>
-  <div class="home">
-
-  </div>
+  <el-container class="home">
+    <el-header class="header">
+      <div class="left">Vue、Element、Spring Boot权限管理系统开发实战</div>
+      <div class="right">右侧</div>
+    </el-header>
+    <el-container class="container">
+      <el-aside class="aside" width="200px">Aside</el-aside>
+      <el-container class="content">
+        <el-main class="main">Main</el-main>
+        <el-footer class="footer"
+          >Copyright © 2020 - 2021 . 秘咖学堂.All Rights Reserved</el-footer
+        >
+      </el-container>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 // @ is an alias to /src
 
 export default {
-  name: 'Home',
-  components: {
+  name: "Home",
+  components: {},
+};
+</script>
+<style lang="scss" scoped>
+.home {
+  width: 100%;
+  height: 100%;
+  background: yellow;
+  display: flex;
+  flex-direction: column;
+  .header {
+    height: 50px;
+    background: blue;
+    display: flex;
+    justify-content: space-between;
+    .left {
+    }
+    .right {
+    }
+  }
 
+  .container {
+    flex: 1;
+
+    .alide {
+      width: 200px;
+      height: 100%;
+      background: red;
+    }
+    .content {
+      flex: 1;
+      background: purple;
+      display: flex;
+      .main {
+        flex: 1;
+      }
+      .footer {
+        height: 50px;
+        background: yellowgreen;
+      }
+    }
   }
 }
-</script>
+</style>
