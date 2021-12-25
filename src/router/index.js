0e1d36c386550c89bbee06b1215d68ef2c8c6ca3
liuzhihua0104 <liuzhihuarose@126.com>
2021-12-25 18:14:00 +0800

    添加路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,39 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-// import Home from '../views/Home.vue'
+import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/home',
-    name: 'home',
-    component: () => import("../views/Home.vue"),
-    children: [
-      {
-        path: '/goodCategory',
-        name: 'goodCategory',
-        component: () => import("@/views/goods/goodsCategory/goodsCategoryList.vue"),
-
-      },
-      {
-        path: '/goodsBrand',
-        name: 'goodsBrand',
-        component: () => import("@/views/goods/goodsBrand/goodsBrandList"),
-      }
-
-    ]
-
-  },
-
-  // {
-  //   path: '/about',
-  //   name: 'About',
-  //   // route level code-splitting
-  //   // this generates a separate chunk (about.[hash].js) for this route
-  //   // which is lazy-loaded when the route is visited.
-  //   component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // },
   {
     path: '/login',
     name: 'login',
@@ -41,7 +12,29 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
-  }
+  },
+  {
+    path: '/home',
+    name: 'home',
+    // component: () => import("../views/Home.vue"),
+    component: Home,
+    children: [
+      {
+        path: '/goodCategory',
+        name: 'goodCategory',
+        component: () => import("../views/goods/goodsCategory/goodsCategoryList.vue"),
+
+      },
+      {
+        path: '/goodsBrand',
+        name: 'goodsBrand',
+        component: () => import("../views/goods/goodsBrand/goodsBrandList"),
+      }
+
+    ]
+
+  },
+ 
 ]
 
 const router = new VueRouter({
